f68341f48dbf8ae376600c732b572d582bfdc643
tony.wang.10101 <tony.wang.10101@gmail.com>
2024-03-14 06:20:43 +0800

    fix: turn off low_cpu_mem_usage

--- llava_13b/1/model.py
+++ llava_13b/1/model.py
@@ -62,7 +62,7 @@ class Llava:
 
         self.model = LlavaLlamaForCausalLM.from_pretrained(
             model_path,
-            low_cpu_mem_usage=True,
+            low_cpu_mem_usage=False,
             device_map="auto",  # "cpu"
             # max_memory={0: "12GB", 1: "12GB", 2: "12GB", 3: "12GB"},
             torch_dtype=torch.float16,
